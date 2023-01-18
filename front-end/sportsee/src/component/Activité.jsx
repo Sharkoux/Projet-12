3a44391b87869efb2794e107fa8451e4e02c5301
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2023-01-18 16:26:02 +0100

    feat: barchart first version OK

--- front-end/sportsee/src/component/Activité.jsx
+++ front-end/sportsee/src/component/Activité.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import {
   BarChart,
@@ -9,13 +8,39 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import styled from "styled-components";
 
+const Tooltips = styled.div`
+  background-color: #E60000;
+  color: white;
+  font-size: 15px;
+  padding: 5px;
+  border: transparent;
+`
+
+const ContainerBarChart = styled.div`
+  background-color: #FBFBFB;
+  width: 835px;
+`
 
 
 export default function Activité({userActivity}) {
  
+  
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <Tooltips>
+        <p className="label">{`${payload[0].value}kg`}</p>
+        <p className="labels">{`${payload[1].value}kcal`}</p>
+      </Tooltips>
+    );
+  }
+
+}
+
   return (
-    
+    <ContainerBarChart>
     <BarChart
       width={835}
       height={320}
@@ -23,26 +48,28 @@ export default function Activité({userActivity}) {
       margin={{
         top: 25,
         right: 30,
-        left: 10,
+        left: 30,
         bottom: 5
       }}
       
     >
-    <Legend verticalAlign="top" align="right"  iconType="circle" iconSize={9} wrapperStyle={{marginRight: "50px", fontSize: "17px", paddingBottom: "35px"}}/>
+    <Legend verticalAlign="top" align="right"  iconType="circle" iconSize={9} wrapperStyle={{marginRight: "50px", fontSize: "17px", paddingBottom: "35px", fontWeight: "bold"}}/>
     <text       x='5%'
                 y='10%'
                 dy={+12}
                 style={{ fontSize: 16, fill: '#20253A', fontWeight: "bold" }}
                 width={200}
-               >Activité quotidienne</text>
+               >Activité quotidienne
+    </text>
       <CartesianGrid  vertical={false} strokeDasharray="2" />
       <XAxis dataKey="day" tickLine={false} domain={[1, 10]} />
       
-      <Tooltip />
+      <Tooltip content={<CustomTooltip />}/>
       
       <Bar barSize={6} name="Poids(kg)" dataKey="kilogram"  fill="#282D30" />
       <Bar barSize={6} name="Calorie brûlées(kCal)" dataKey="calories" fill="#E60000" />
       <YAxis orientation="right"  tickLine={false} axisLine={false}  />
     </BarChart>
+    </ContainerBarChart>
   );
 }
